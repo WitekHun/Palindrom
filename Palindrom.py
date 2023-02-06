aa0401e351ae1f4bb86a86129fd42a360361f395
Witold Hungendorfer <witekh@me.com>
2023-02-06 14:41:26 +0100

    Add comment

--- Palindrom.py
+++ Palindrom.py
@@ -1,8 +1,14 @@
 def palindrom_test(ward):
+    """
+       Fukcja do sprawdzania czy dany wyraz jest palindromem
+       argument funkcji naley podawać w "" lub ''
+       wyrazy mogą być pisane z duej lub małej litery
+       funkcja zwraca wartość boolean: True/False
+    """
     ward_revers = ''
     ward = str(ward)
     for i in range(1,len(ward)+1):
         ward_revers += ward[-i]
     print(ward_revers.lower() == ward.lower())
-    print(ward_revers.lower())
+    return(ward_revers.lower() == ward.lower())
 palindrom_test('Witek')
