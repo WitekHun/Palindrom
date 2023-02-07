f56b674297e705c191bed6a84b34428cf5e059d4
Witold Hungendorfer <witekh@me.com>
2023-02-07 09:06:40 +0100

    Add ż

--- Palindrom.py
+++ Palindrom.py
@@ -1,11 +1,11 @@
 def palindrom_test(ward):
     """
        Fukcja do sprawdzania czy dany wyraz jest palindromem
-       argument funkcji naley podawać w "" lub ''
-       wyrazy mogą być pisane z duej lub małej litery
+       argument funkcji należy podawać w "" lub ''
+       wyrazy mogą być pisane z dużej lub małej litery
        funkcja zwraca wartość boolean: True/False
     """
-    ward_revers = ward[len(ward)::-1]
+    ward_revers = ward[::-1]
     return ward_revers.lower() == ward.lower()
 
 print("Słowo %s jest palindromem: %s" %('Witek',palindrom_test('Witek')))
